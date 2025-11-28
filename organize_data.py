589e14bc41b7fd6be5c7febe25c616aa53c66172
riccimason99 <144854189+riccimason99@users.noreply.github.com>
2025-11-28 13:31:35 +0000

    Initial commit: NFL play classification ML project

---
+++ organize_data.py
@@ -0,0 +1,32 @@
+# Target: PlayOutcome: Run (QB or Running Back), Short Pass, Long Pass, Punt, Field Goal 
+# (drop where playstart is NULL, sacs)
+# Features:
+# ====DIRECTLY FROM DATASET====
+# - Season: The NFL season the play belongs to.
+# - Quarter: The quarter of the game the play was attempted.
+# - TeamWithPossession: Team with possession that attempted the play.
+# - PlayNumberInDrive: Play number during the drive that the play was attempted.
+
+
+# - PlayTimeFormation ?????: Time left in quarter / Quarter play took place / formation of offense
+# - Season: The NFL season the play belongs to.
+# - Time Remaining
+# - HomeTeam: Name of the home team.
+# - Date: Exact date (month and day) of the game.
+
+
+
+# ====ENGINEERED FEATURES====
+# - Foal_Weather??: Booliean Created with "Home Team", Date, Indoor/Outdoor Stadium 
+# - Score Difference: 
+# - Team/Season 
+# - Was Sucessful - Yard gained yes or no? 
+# - Time Left in Quarter: 
+# - Yards from endzone ()
+
+
+# AwayTeam: Name of the visiting team.
+
+
+# - PlayStart -> Yards to endzone, down (1234), yard to first down
+
